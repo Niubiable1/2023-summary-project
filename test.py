39842ee9a01c9088ea983c5be842055e72dd7f34
wciltea <125111866+wciltea@users.noreply.github.com>
2023-08-25 06:42:34 +0000

    docstrings, repetition

--- test.py
+++ test.py
@@ -8,38 +8,59 @@ class Test:
 
 
     def select_ag(self):
+        '''selects all the agents in the list'''
         for a in range(5):
             try:
                 print(p.agent_select(a))
 
             except IndexError:
                 print('No more agents')
-    def select_map(self):    
-        p.map_select(random.randint(0,2))    
-
+                
+    def select_map(self):
+        '''selects all maps in the list'''
+        for a in range(3):
+            try:
+                p.map_select(a)    
+            except IndexError:
+                print('No more maps')
+                
     def omen_ab(self):
+        '''teleports to a random location 10 times per map, 3 maps'''
         maps = ['ascent', 'bind', 'haven']
-        b = random.randint(0,3)
-        p.map_select(b) 
-        p.initialise('omen')   
-        a = random.randint(0,len(data.make_map(maps[b])))
-        print(list(data.make_map('ascent').keys()),', ',a+1,'\nYou teleported to', p.player_pos.get_paths()[a])
-        p.omen(a)
-
+        for b in range(3):
+            print(maps[b][0].upper()+maps[b][1:],'\n')
+            p.map_select(b) 
+            data.make_map(maps[b])
+            p.initialise('omen')  
+            for c in range(10):
+                try:
+                    a = random.randint(0,len(p.player_pos.get_paths())-1)
+                    print('You teleported to', p.player_pos.get_paths()[a],'\n')
+                    p.omen(a)
+                except IndexError:
+                    print('No more rooms')
     def prompt_test(self):
         p.prompt(['Option A', 'Option B', 'Option C','Option 4'],'Test input', False)
 
     def sage_ab(self):
-        a = random.randint(0,len(p.player_pos.get_paths()))
-        print(p.player_pos.get_paths(), ', ', a + 1)
-        p.sage(a)
-
+        '''blocks off all paths to the current room'''
+        for a in range(5):
+            try:
+                print(p.player_pos.get_paths())
+                p.sage(0)
+            except IndexError:
+                print('No other paths to block\n')
+                break
+                
     def sova_ab(self):
-        a = random.randint(0,len(p.player_pos.get_paths()))
-        print(p.player_pos.get_paths(), ', ', a + 1)
-        p.sova(a)
-        pass
-    
+        '''reveals information in all paths'''
+        for a in range(5):
+            try:
+                print('\n'+str(p.player_pos.get_paths()), ', ', a + 1)
+                p.sova(a)
+            except IndexError:
+                print('No more paths\n\n')
+                break
 
 p = game.Game()
 x = Test()
@@ -47,5 +68,5 @@ x.select_ag()
 x.select_map()
 x.prompt_test()
 x.omen_ab()
-x.sage_ab()
 x.sova_ab()
+x.sage_ab()
