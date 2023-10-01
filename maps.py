26e1dbb912901dab63646ecd559ccc7c11ffdf9a
ngjunsiang <45561895+ngjunsiang@users.noreply.github.com>
2023-10-01 14:30:28 +0000

    Move map data to json file

--- maps.py
+++ maps.py
@@ -1,6 +1,15 @@
 import data
 
 
+DATAFILE = "mapdata.json"
+
+
+def load_json(filename: str) -> dict:
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data
+
+
 class Map:
     """Encapsulates a set of connected rooms.
 
@@ -39,71 +48,8 @@ def make_map(name: str) -> Map:
     It then returns the dictionary of room objects for
     the map if the map name is valid, else it returns -1
     """
-    if name == 'ascent':
-        _roompaths = {
-            "T-side spawn": ["A lobby", "B lobby"],
-            "A lobby": ["T-side spawn", "A main", "Catwalk"],
-            "A main": ["A lobby", "A site"],
-            "A site": ["A main", "Garden", "CT-side spawn"],
-            "B site": ["CT-side spawn", "Market", "B main"],
-            "B main": ["B site", "B lobby"],
-            "B lobby": ["T-side spawn", "Tiles", "B main"],
-            "Tiles": ["B lobby", "Catwalk", "Garden", "Market"],
-            "Catwalk": ["A lobby", "Tiles", "Market", "Garden"],
-            "Garden": ["A site", "Market", "Catwalk", "Tiles"],
-            "Market":
-            ["CT-side spawn", "B site", "Tiles", "Catwalk", "Garden"],
-            "CT-side spawn": ["A site", "Market", "B site"]
-        }
-    elif name == 'haven':
-        _roompaths = {
-            "T-side spawn": ["A garden", "Grass", "C lobby"],
-            "A garden": ["T-side spawn", "Mid window", "A lobby"],
-            "A lobby": ["A garden", "A short", "A long"],
-            "A long": ["A lobby", "A site"],
-            "A short": ["A lobby", "A site"],
-            "A site": ["A long", "A short", "A link"],
-            "A link": ["A site", "Heaven", "B site", "CT-side spawn"],
-            "Heaven": ["A link", "A site"],
-            "B site": ["A link", "Mid", "C link"],
-            "C link": ["B site", "Garage window", "C site", "CT-side spawn"],
-            "C site": ["C link", "C long", "Garage"],
-            "C long": ["C site", "C lobby"],
-            "Mid": ["Mid window", "B site", "Grass"],
-            "Mid window": ["Mid", "A garden"],
-            "Grass": ["T-side spawn", "Mid", "Garage"],
-            "Garage": ["C site", "Garage window", "Grass"],
-            "Garage window": ["Garage", "C link"],
-            "C lobby": ["T-side spawn", "C long"],
-            "CT-side spawn": ["A link", "C link"]
-        }
-    elif name == 'bind':
-        _roompaths = {
-            "T-side spawn": ["T-side cave", "A lobby", "Market"],
-            "T-side cave": ["T-side spawn", "A lobby", "Market"],
-            "A lobby": ["T-side spawn", "T-side cave", "A short", "Showers"],
-            "Market":
-            ["A short", "B main", "T-side spawn", "T-side cave", "Fountain"],
-            "A short": ["A lobby", "Market"],
-            "A tp exit": ["A lobby"],
-            "Showers": ["A lobby", "A site"],
-            "A site":
-            ["Showers", "A short", "B tp exit", "U-hall", "CT-side spawn"],
-            "U-hall": ["A site"],
-            "Heaven": ["A site", "CT-side spawn"],
-            "B hall": ["CT-side spawn", "B site", "B elbow"],
-            "B site": ["B hall", "B elbow", "Hookah", "Garden"],
-            "B elbow": ["B site", "B hall"],
-            "Hookah": ["B main", "B site"],
-            "B main": ["Market", "Fountain", "Hookah"],
-            "Garden": ["B site", "B long", "A tp exit"],
-            "B long": ["Fountain", "Garden", "A tp exit"],
-            "Fountain": ["Market", "B main", "B long"],
-            "B tp exit": ["B main"],
-            "CT-side spawn": ["Heaven", "B hall"]
-        }
-    else:
-        raise ValueError(f"{name}: no such map")
-
-    return Map(_roompaths)
+    mapdata = load_json(DATAFILE)
+    if name.lower() in mapdata:
+        return Map(mapdata[name.lower()])
+    raise ValueError(f"{name}: no such map")
 
